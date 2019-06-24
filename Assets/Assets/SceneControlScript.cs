678f8efde9a8e3dfc2cf9087577e4741c173f9b1
Riccardo <smerilliriccardo@gmail.com>
2019-06-24 11:17:56 +0200

    SceneControl e Music Themes
    - Definiti metodi per la fase di sconfitta e di vittoria (di quest'ultima, da modificare con le cutscene finali)
    - Modifica parziali dei valori del gameplay (in particolare, aggiunto valore dell'ampiezza del moto locale dei nemici)
    - Aggiunte le musiche per la sfida con il Boss (BossTheme), per la vittoria (VictoryTheme) e per la sconfitta (DeathTheme).

--- Assets/Assets/SceneControlScript.cs
+++ Assets/Assets/SceneControlScript.cs
@@ -7,6 +7,8 @@ public class SceneControlScript : MonoBehaviour
 {
     Transform pauseMenu;
     private bool inPause;
+    private bool gameOver;
+    private bool gameVictory;
     private GameObject player;
     private GameObject[] enemies;
     private GameObject boss;
@@ -15,28 +17,37 @@ public class SceneControlScript : MonoBehaviour
     private Wave wave;
     private BackgroundMovement[] background;
 
+    public AudioClip mainTheme;
+    public AudioClip bossTheme;
+    public AudioClip deathTheme;
+    public AudioClip victoryTheme;
+
     private Level currentLevel;
 
     // Start is called before the first frame update
     void Awake()
     {
         inPause = false;
+        gameOver = false;
+        gameVictory = false;
         pauseMenu = GameObject.Find("PauseMenu").transform;
         player = GameObject.FindGameObjectWithTag("Player");
         wave = GameObject.Find("Wave").GetComponent<Wave>();
         background = GameObject.Find("Backgrounds").GetComponentsInChildren<BackgroundMovement>();
         currentLevel = new Level(1);
+
+        GetComponent<AudioSource>().PlayOneShot(mainTheme);
     }
 
     // Update is called once per frame
     void Update()
     {
-        PauseClick();
+        PauseMode();
     }
 
-    private void PauseClick ()
+    private void PauseMode ()
     {
-        if (Input.GetButtonDown("Cancel"))
+        if (Input.GetButtonDown("Cancel") && !gameOver && !gameVictory)
         {
             enemies = GameObject.FindGameObjectsWithTag("Enemy");
             bullets = GameObject.FindGameObjectsWithTag("Bullet");
@@ -95,10 +106,53 @@ public class SceneControlScript : MonoBehaviour
     {
         Application.Quit();
     }
-
-    public void SetValues()
+    
+    public void GameOverMode()
     {
-        
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        bullets = GameObject.FindGameObjectsWithTag("Bullet");
+
+        if (GameObject.Find("Boss") != null)
+            boss = GameObject.Find("Boss").transform.GetChild(0).gameObject;
+        else
+            boss = null;
+
+        gameOver = true;
+        pauseMenu.GetChild(0).gameObject.SetActive(true);
+        pauseMenu.GetChild(2).gameObject.SetActive(true);
+
+        for (int i = 0; i < enemies.Length; i++)
+            enemies[i].GetComponent<EnemyClass>().PauseOn();
+        for (int i = 0; i < bullets.Length; i++)
+            bullets[i].GetComponent<BulletClass>().PauseOn();
+        player.GetComponent<PlayerClass>().PauseOn();
+        if (boss != null)
+            boss.GetComponent<BossClass>().PauseOn();
+        wave.PauseOn();
+        for (int i = 0; i < background.Length; i++)
+            background[i].PauseOn();
+
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().PlayOneShot(deathTheme);
+        GetComponent<AudioSource>().loop = false;
+    }
+
+    /* SOSTITUIRE CON IMMAGINI POST VICTORY */
+    public void GameVictoryMode()
+    {
+        gameVictory = true;
+        pauseMenu.GetChild(0).gameObject.SetActive(true);
+        pauseMenu.GetChild(3).gameObject.SetActive(true);
+
+        player.GetComponent<PlayerClass>().PauseOn();
+
+        wave.PauseOn();
+        for (int i = 0; i < background.Length; i++)
+            background[i].PauseOn();
+
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().PlayOneShot(victoryTheme);
+        GetComponent<AudioSource>().loop = false;
     }
 
     public Level CurrentLevel
@@ -121,19 +175,23 @@ public struct Level
 
     float enemyDashSpeed;
     float enemyDashFrequency;
+    float enemyDashAmplitude;
     float enemyDashCooldown;
 
     float enemyCircleSpeed;
     float enemyCircleFrequency;
+    float enemyCircleAmplitude;
     float enemyCircleCooldown;
 
     float enemyChargeSpeed;
 
     bool enemyBossSpawn;
     float enemyBossFrequency;
+    float enemyBossAmplitude;
     float enemyBossCooldown;
     float enemyBossRushSpeed;
     float enemyBossRushFrequency;
+    float enemyBossRushAmplitude;
     float enemyBossRushCooldown;
 
 
@@ -147,16 +205,20 @@ public struct Level
         enemyLineCooldown = 0f;
         enemyDashSpeed = 0f;
         enemyDashFrequency = 0f;
+        enemyDashAmplitude = 0f;
         enemyDashCooldown = 0f;
         enemyCircleSpeed = 0f;
         enemyCircleFrequency = 0f;
+        enemyCircleAmplitude = 0f;
         enemyCircleCooldown = 0f;
         enemyChargeSpeed = 0f;
         enemyBossSpawn = false;
         enemyBossFrequency = 0f;
+        enemyBossAmplitude = 0f;
         enemyBossCooldown = 0f;
         enemyBossRushSpeed = 0f;
         enemyBossRushFrequency = 0f;
+        enemyBossRushAmplitude = 0f;
         enemyBossRushCooldown = 0f;
 
         switch (number)
@@ -169,10 +231,12 @@ public struct Level
                     enemyLineSpeed = 0.5f;
                     enemyLineCooldown = 3f;
                     enemyDashSpeed = 0.5f;
-                    enemyDashFrequency = 1f;
+                    enemyDashFrequency = 7f;
+                    enemyDashAmplitude = 0.3f;
                     enemyDashCooldown = 5f;
                     enemyCircleSpeed = 0.5f;
-                    enemyCircleFrequency = 1f;
+                    enemyCircleFrequency = 6.5f;
+                    enemyCircleAmplitude = 0.4f;
                     enemyCircleCooldown = 5f;
                     enemyChargeSpeed = 0.5f;
                 }
@@ -186,10 +250,12 @@ public struct Level
                     enemyLineSpeed = 0.5f;
                     enemyLineCooldown = 2f;
                     enemyDashSpeed = 1f;
-                    enemyDashFrequency = 1.5f;
+                    enemyDashFrequency = 7f;
+                    enemyDashAmplitude = 0.3f;
                     enemyDashCooldown = 3f;
                     enemyCircleSpeed = 1f;
-                    enemyCircleFrequency = 1.5f;
+                    enemyCircleFrequency = 6.5f;
+                    enemyCircleAmplitude = 0.4f;
                     enemyCircleCooldown = 3f;
                     enemyChargeSpeed = 1f;
                 }
@@ -200,15 +266,17 @@ public struct Level
                     minSpawn = 6;
                     maxSpawn = 8;
                     cooldownSpawn = 4f;
-                    enemyLineSpeed = 0.8f;
+                    enemyLineSpeed = 0.6f;
                     enemyLineCooldown = 2f;
-                    enemyDashSpeed = 2f;
-                    enemyDashFrequency = 2f;
+                    enemyDashSpeed = 1.2f;
+                    enemyDashFrequency = 7f;
+                    enemyDashAmplitude = 0.3f;
                     enemyDashCooldown = 2f;
-                    enemyCircleSpeed = 2f;
-                    enemyCircleFrequency = 2f;
+                    enemyCircleSpeed = 1.2f;
+                    enemyCircleFrequency = 7f;
+                    enemyCircleAmplitude = 0.3f;
                     enemyCircleCooldown = 2f;
-                    enemyChargeSpeed = 2f;
+                    enemyChargeSpeed = 1.5f;
                 }
                 break;
 
@@ -220,18 +288,22 @@ public struct Level
                     enemyLineSpeed = 0.5f;
                     enemyLineCooldown = 2f;
                     enemyDashSpeed = 1f;
-                    enemyDashFrequency = 1.5f;
+                    enemyDashFrequency = 7f;
+                    enemyDashAmplitude = 0.3f;
                     enemyDashCooldown = 3f;
                     enemyCircleSpeed = 1f;
-                    enemyCircleFrequency = 1.5f;
+                    enemyCircleFrequency = 6.5f;
+                    enemyCircleAmplitude = 0.4f;
                     enemyCircleCooldown = 3f;
                     enemyChargeSpeed = 1f;
                     enemyBossSpawn = true;
                     enemyBossFrequency = 0.8f;
+                    enemyBossAmplitude = 0.5f;
                     enemyBossCooldown = 4f;
                     enemyBossRushSpeed = 1.5f;
                     enemyBossRushFrequency = 3f;
-                    enemyBossRushCooldown = 5f;
+                    enemyBossRushAmplitude = 0.5f;
+                    enemyBossRushCooldown = 8f;
                 }
                 break;
         }
@@ -245,15 +317,19 @@ public struct Level
     public float EnemyLineCooldown { get => enemyLineCooldown; }
     public float EnemyDashSpeed { get => enemyDashSpeed; }
     public float EnemyDashFrequency { get => enemyDashFrequency; }
+    public float EnemyDashAmplitude { get => enemyDashAmplitude; }
     public float EnemyDashCooldown { get => enemyDashCooldown; }
     public float EnemyCircleSpeed { get => enemyCircleSpeed; }
     public float EnemyCircleFrequency { get => enemyCircleFrequency; }
+    public float EnemyCircleAmplitude { get => enemyCircleAmplitude; }
     public float EnemyCircleCooldown { get => enemyCircleCooldown; }
     public float EnemyChargeSpeed { get => enemyChargeSpeed; }
     public bool EnemyBossSpawn { get => enemyBossSpawn; }
     public float EnemyBossFrequency { get => enemyBossFrequency; }
+    public float EnemyBossAmplitude { get => enemyBossAmplitude; }
     public float EnemyBossCooldown { get => enemyBossCooldown; }
     public float EnemyBossRushSpeed { get => enemyBossRushSpeed; }
     public float EnemyBossRushFrequency { get => enemyBossRushFrequency; }
+    public float EnemyBossRushAmplitude { get => enemyBossRushAmplitude; }
     public float EnemyBossRushCooldown { get => enemyBossRushCooldown; }
 }
